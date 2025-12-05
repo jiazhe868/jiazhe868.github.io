4e2c4233b66a11e0a6379fb30b1b7d4632614855
jiazhe868 <31788272+jiazhe868@users.noreply.github.com>
2025-12-05 05:33:57 +0000

    Deploying to gh-pages from @ jiazhe868/jiazhe868.github.io@57d0a94c274cf6865748fc85d18814774fd8edb5 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -38,8 +38,8 @@ ninja.data = [{
             window.location.href = "/repositories/";
           },
         },{id: "nav-cv",
-          title: "cv",
-          description: "This is a description of the page. You can modify it in &#39;_pages/cv.md&#39;. You can also change or remove the top pdf download button.",
+          title: "CV",
+          description: "Zhe Jia&#39;s Curriculum Vitae",
           section: "Navigation",
           handler: () => {
             window.location.href = "/cv/";
